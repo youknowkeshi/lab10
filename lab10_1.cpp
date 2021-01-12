fdc314460a55e31e47f6e7fee7a7f4be387ddcfb
youknowkeshi <76035490+youknowkeshi@users.noreply.github.com>
2021-01-12 17:40:39 +0700

    กินข้าวยัง

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<ctime>
+using namespace std;
+int main()
+{
+	cout << "Press Enter 3 times to reveal your future.";
+	srand(time(0));
+	cin.get();
+	cin.get();
+	cin.get();
+	string x[] = {"A","B+","B","C+","C","D+","D","F","W"};
+	int i = rand()%9;
+	cout << "You will get " << x[i] << " " << "in this 261102." ;
+}
